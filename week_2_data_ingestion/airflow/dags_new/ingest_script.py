226d62df466ec22158d03714343e8e9b757706e0
mbenfredj <benfredjmajdi@gmail.com>
2024-11-08 12:21:38 +0100

    ingest data to local pgsql

---
+++ week_2_data_ingestion/airflow/dags_new/ingest_script.py
@@ -0,0 +1,41 @@
+import pandas as pd
+from sqlalchemy import create_engine
+from time import time
+
+def ingest_callable(user, password, host, port, db, table_name, csv_file, execution_date):
+    print(table_name, csv_file, execution_date)
+
+    engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
+    engine.connect()
+
+    print('connection established successfully, inserting data...')
+
+    t_start = time()
+    df_iter = pd.read_csv(csv_file, iterator=True, chunksize=100000)
+
+    df = next(df_iter)
+
+    df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+    df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+
+    df.head(n=0).to_sql(name=table_name, con=engine, if_exists="replace")
+
+    df = df.to_sql(name=table_name, con=engine, if_exists="append")
+    
+    t_end = time()
+    print('inserted the first chunk, took %.3f second' % (t_end - t_start))
+
+    try:
+
+        while True:
+            t_start = time()
+            df = next(df_iter)
+            df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+            df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+            df = df.to_sql(name=table_name, con=engine, if_exists="append")
+            t_end = time()
+            print("inserted another chunk..., took %.3f second " % (t_end - t_start))
+    except StopIteration:
+        print("All data has been processed.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
